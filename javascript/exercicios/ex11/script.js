5c53c7162cd12ca41a9c2ec491f5c4d0e139a0ba
Virgínia Arruda <vica_arruda@hotmail.com>
2022-01-31 17:09:15 -0300

    exercicio 11 - estrutura if/else if/else

---
+++ javascript/exercicios/ex11/script.js
@@ -0,0 +1,11 @@
+let idade = 18;
+let cnh = false;
+
+if(idade >= 18 && cnh == true) {
+    console.log("Sim, pode dirigir");
+} else if(idade >= 18 && cnh == false) {
+    console.log("Não pode dirigir, mas pode fazer a CNH");
+} else {
+    console.log("Não pode dirigir");
+}
+
