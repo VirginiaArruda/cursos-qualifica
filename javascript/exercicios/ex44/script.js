e644e2dc08f7d9b1dc2004d651b7ff477a6cab8a
Virgínia Arruda <vica_arruda@hotmail.com>
2022-02-12 12:23:56 -0300

    exercicio 44 - validando apenas letras maiusculas

---
+++ javascript/exercicios/ex44/script.js
@@ -0,0 +1,8 @@
+const validarMaiuscula = /[A-Z]/;
+
+console.log(validarMaiuscula.test("testando"));
+console.log(validarMaiuscula.test("123"));
+console.log(validarMaiuscula.test("TESTANDO"));
+
+
+
